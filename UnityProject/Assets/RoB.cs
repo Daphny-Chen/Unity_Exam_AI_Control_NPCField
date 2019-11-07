59db9093b305652f6f96deac94985d6216c76148
Daphny-Chen <55218517+Daphny-Chen@users.noreply.github.com>
2019-11-07 11:25:23 +0800

    控制移動與任務建立

---
+++ UnityProject/Assets/RoB.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class RoB : MonoBehaviour
+{
+
+
+    [Header("速度"), Range(0f, 1000f)]
+    public float speed = 50f;
+    [Header("跳躍"), Range(0f, 1000f)]
+    public float jump = 100f;
+    [Header("是否在地上")]
+    public bool IsGround = false;
+
+    private Rigidbody r2d;
+
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();
+        Jump();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsGround = true;
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
+        //向前
+        if (Input.GetKey(KeyCode.W))
+        { transform.Translate(0, 0, speed * 0.1f * Time.deltaTime); }
+        //向後
+        if (Input.GetKey(KeyCode.S))
+        { transform.Translate(0, 0, speed * -0.1f * Time.deltaTime); }
+        //向右
+        if (Input.GetKey(KeyCode.D))
+        { transform.Translate(speed * 0.1f * Time.deltaTime,0,  0); }
+        //向左
+        if (Input.GetKey(KeyCode.A))
+        { transform.Translate(speed * -0.1f * Time.deltaTime,0, 0); }
+    }
+
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && IsGround == true)
+        {
+            r2d.AddForce(new Vector3(0, jump,0));
+            IsGround = false;
+        }
+    }
+}
---
+++ UnityProject/Assets/RoP.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoP : MonoBehaviour {
+
+    [Header("對話")]
+    public string Say = "嗨，你好，我快沒電了，可以請你幫我找到五顆電池？";
+    public float Speed = 0.2f;
+    [Header("任務相關")]
+    public bool Complete = false;
+    public int BateryFinush = 5;
+    public int Batery = 0;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
